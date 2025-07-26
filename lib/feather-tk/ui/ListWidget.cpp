b7b9e2a04a227e785cdef0d6c45052811d57b7a7
Darby Johnston <darbyjohnston@yahoo.com>
2025-07-26 15:25:26 -0700

    Revert change

--- lib/feather-tk/ui/ListWidget.cpp
+++ lib/feather-tk/ui/ListWidget.cpp
@@ -119,16 +119,6 @@ namespace feather_tk
         _p->scrollWidget->setGeometry(value);
     }
 
-    bool ListWidget::hasBorder() const
-    {
-        return _p->scrollWidget->hasBorder();
-    }
-
-    void ListWidget::setBorder(bool value)
-    {
-        _p->scrollWidget->setBorder(value);
-    }
-
     void ListWidget::sizeHintEvent(const SizeHintEvent& event)
     {
         IWidget::sizeHintEvent(event);
