e6b1a7555250a83a20795b96e2d5fdb7b6c54728
Darby Johnston <darbyjohnston@yahoo.com>
2025-08-20 12:30:40 -0700

    Build fixes

--- etc/Linux/linux-build-gha.sh
+++ etc/Linux/linux-build-gha.sh
@@ -14,10 +14,7 @@ cmake \
     -DCMAKE_PREFIX_PATH=$PWD/install-$BUILD_TYPE \
     -Dfeather_tk_API=$FEATHER_TK_API \
     -Dfeather_tk_nfd=$FEATHER_TK_NFD \
-    -Dfeather_tk_PYTHON=$FEATHER_TK_PYTHON \
-    -Dfeather_tk_TESTS=$FEATHER_TK_TESTS \
-    -Dfeather_tk_EXAMPLES=$FEATHER_TK_EXAMPLES \
-    -Dfeather_tk_GCOV=$FEATHER_TK_GCOV
+    -Dfeather_tk_PYTHON=$FEATHER_TK_PYTHON
 cmake --build superbuild-$BUILD_TYPE -j $JOBS --config $BUILD_TYPE
 
 cmake \
@@ -25,6 +22,12 @@ cmake \
     -B build-$BUILD_TYPE \
     -DCMAKE_INSTALL_PREFIX=$PWD/install-$BUILD_TYPE \
     -DCMAKE_PREFIX_PATH=$PWD/install-$BUILD_TYPE \
-    -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -Dfeather_tk_API=$FEATHER_TK_API \
+    -Dfeather_tk_nfd=$FEATHER_TK_NFD \
+    -Dfeather_tk_PYTHON=$FEATHER_TK_PYTHON \
+    -Dfeather_tk_TESTS=$FEATHER_TK_TESTS \
+    -Dfeather_tk_EXAMPLES=$FEATHER_TK_EXAMPLES \
+    -Dfeather_tk_GCOV=$FEATHER_TK_GCOV
 cmake --build build-$BUILD_TYPE -j $JOBS --config $BUILD_TYPE
 cmake --build build-$BUILD_TYPE --config $BUILD_TYPE --target install
--- etc/Windows/windows-build-gha.bat
+++ etc/Windows/windows-build-gha.bat
@@ -3,15 +3,14 @@ set BUILD_TYPE=%1
 set JOBS=4
 
 cmake ^
+    -S feather-tk/etc/SuperBuild ^
+    -B superbuild-%BUILD_TYPE% ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -DCMAKE_INSTALL_PREFIX=install-%BUILD_TYPE% ^
     -DCMAKE_PREFIX_PATH=install-%BUILD_TYPE% ^
     -Dfeather_tk_API=%FEATHER_TK_API% ^
     -Dfeather_tk_nfd=%FEATHER_TK_NFD% ^
-    -Dfeather_tk_PYTHON=%FEATHER_TK_PYTHON% ^
-    -Dfeather_tk_TESTS=%FEATHER_TK_TESTS% ^
-    -Dfeather_tk_EXAMPLES=%FEATHER_TK_EXAMPLES% ^
-    -Dfeather_tk_GCOV=%FEATHER_TK_GCOV%
+    -Dfeather_tk_PYTHON=%FEATHER_TK_PYTHON%
 cmake --build superbuild-%BUILD_TYPE% -j %JOBS% --config %BUILD_TYPE%
 
 cmake ^
@@ -19,6 +18,12 @@ cmake ^
     -B build-%BUILD_TYPE% ^
     -DCMAKE_INSTALL_PREFIX=%CD%/install-%BUILD_TYPE% ^
     -DCMAKE_PREFIX_PATH=%CD%/install-%BUILD_TYPE% ^
-    -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
+    -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+    -Dfeather_tk_API=%FEATHER_TK_API% ^
+    -Dfeather_tk_nfd=%FEATHER_TK_NFD% ^
+    -Dfeather_tk_PYTHON=%FEATHER_TK_PYTHON% ^
+    -Dfeather_tk_TESTS=%FEATHER_TK_TESTS% ^
+    -Dfeather_tk_EXAMPLES=%FEATHER_TK_EXAMPLES% ^
+    -Dfeather_tk_GCOV=%FEATHER_TK_GCOV%
 cmake --build build-%BUILD_TYPE% -j %JOBS% --config %BUILD_TYPE%
 cmake --build build-%BUILD_TYPE% --config %BUILD_TYPE% --target install
