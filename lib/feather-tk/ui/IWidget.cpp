702579d5759961143cefbbccf4fa9d52d5430060
Darby Johnston <darbyjohnston@yahoo.com>
2025-08-24 10:23:36 -0700

    Focus fix

--- lib/feather-tk/ui/IWidget.cpp
+++ lib/feather-tk/ui/IWidget.cpp
@@ -312,7 +312,7 @@ namespace feather_tk
 
     void IWidget::takeKeyFocus()
     {
-        if (_enabled)
+        if (isEnabled())
         {
             if (auto window = getWindow())
             {
